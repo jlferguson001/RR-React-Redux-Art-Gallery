33938fbdf08ef8444ed1996d42cf5ecfe09242be
Jenni Ferguson <98863330+jlferguson001@users.noreply.github.com>
2022-07-24 13:22:28 -0500

    middleware

--- src/store.js
+++ src/store.js
@@ -1,12 +1,14 @@
 import { configureStore} from '@reduxjs/toolkit'
 //pluging in the slice
-// import dataReducer from './features/dataSlice'
-import modeReducer from './features/modeSlice'
+import dataReducer from './features/dataSlice'
+// import modeReducer from './features/modeSlice'
+import {logger} from './features/middleware'
 
 export const store = configureStore({
     reducer: {
   //plugging in the slice
-//   data: dataReducer,   
-  mode: modeReducer   
-    }
+  data: dataReducer,   
+//   mode: modeReducer   
+    },
+    middleware: [logger]
 });
