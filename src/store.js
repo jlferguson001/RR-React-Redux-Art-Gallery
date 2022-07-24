db7a03e7d064e2a29bd5bdf5132ce0d646b62a8c
Jenni Ferguson <98863330+jlferguson001@users.noreply.github.com>
2022-07-24 12:47:52 -0500

    darkmode and lightmode

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { configureStore} from '@reduxjs/toolkit'
 //pluging in the slice
-import dataReducer from './features/dataSlice'
+// import dataReducer from './features/dataSlice'
+import modeReducer from './features/modeSlice'
 
 export const store = configureStore({
     reducer: {
   //plugging in the slice
-  data: dataReducer      
+//   data: dataReducer,   
+  mode: modeReducer   
     }
-})
+});
